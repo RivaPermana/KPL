68405a56701b67abf3a9dee60f3aa56c508e7b89
shad kinx <rivapermana763@gmail,com>
2024-02-27 14:44:43 +0700

    test

---
+++ KPLW2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace asd
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Wasap");
+        }
+    }
+}
